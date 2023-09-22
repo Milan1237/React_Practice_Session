dc43f14e9cb37b8b0ac1b74c89af0b4d355f4b8a
Milan Das <Milan1237das@gmail.com>
2023-09-23 00:32:36 +0530

    just created react project for the first time

---
+++ 01basicReact/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Hello Milan, I am trying to learn react</h1>
+  )
+}
+
+export default App
